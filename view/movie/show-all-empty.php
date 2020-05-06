cdb993f5b3bc63e2bbdac38c695b67475169e989
Björn Olsson <bjorn.olsson87@gmail.com>
2020-05-06 11:59:32 +0100

    Added style for movie database

---
+++ view/movie/show-all-empty.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Anax\View;
+
+/**
+ * Render content within an article.
+ */
+
+// Show incoming variables and view helper functions
+//echo showEnvironment(get_defined_vars(), get_defined_functions());
+?>
+
+<div class="movieIndex">
+    <h2>Databasen är tom</h2>
+</div>
